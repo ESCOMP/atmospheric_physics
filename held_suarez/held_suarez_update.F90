33a1ce43d8486a57b4542cf08f7ea060bc58816d
Courtney Peverley <courtneyp@izumi.unified.ucar.edu>
2021-07-23 14:42:39 -0600

    adding qneg to HS1994 sdf

--- held_suarez/held_suarez_update.F90
+++ held_suarez/held_suarez_update.F90
@@ -16,7 +16,8 @@ contains
 
 !> \section arg_table_held_suarez_update_run Argument Table
 !! \htmlinclude held_suarez_update_run.html
-  subroutine held_suarez_update_run(dt,cpair,du,dv,s,u,v,t,errmsg,errflg)
+  subroutine held_suarez_update_run(dt,cpair,du,dv,s,u,v,t,subnam,errmsg,     &
+    errflg)
 
     real(kind_phys),   intent(in)   :: dt        ! time step
     real(kind_phys),   intent(in)   :: cpair
@@ -26,11 +27,13 @@ contains
     real(kind_phys),   intent(inout):: u(:,:)    ! Zonal wind (m/s)
     real(kind_phys),   intent(inout):: v(:,:)    ! Meridional wind (m/s)
     real(kind_phys),   intent(inout):: t(:,:)    ! Temperature (K)
+    character(len=64), intent(out)  :: subnam
     character(len=512),intent(out)  :: errmsg
     integer,           intent(out)  :: errflg
 
     errmsg = ' '
     errflg = 0
+    subnam = "HELD_SUAREZ_1994"
 
     ! Add the tendencies to the state variables
     u(:,:) = u(:,:) + du(:,:) * dt
