49d929aaf51ddeb9af0dab94810d149780baeaf6
Steve Goldhaber <goldy@ucar.edu>
2021-09-17 20:17:00 -0600

    Move update_dry_static_energy to new module

--- utilities/physics_tendency_updaters.F90
+++ utilities/physics_tendency_updaters.F90
@@ -9,12 +9,6 @@ module physics_tendency_updaters
   public :: apply_tendency_of_y_wind_run
   public :: apply_heating_rate_run
   public :: apply_tendency_of_air_temperature_run
-  public :: update_dry_static_energy_init
-  public :: update_dry_static_energy_run
-
-  ! Private module variables
-
-  real(kind_phys) :: gravit = -HUGE(1.0_kind_phys)
 
 CONTAINS
 
@@ -122,42 +116,5 @@ CONTAINS
       end do
 
    end subroutine apply_tendency_of_air_temperature_run
-
-   !> \section arg_table_update_dry_static_energy_init  Argument Table
-   !! \htmlinclude update_dry_static_energy_init.html
-   subroutine update_dry_static_energy_init(gravit_in)
-      real(kind_phys),    intent(in)    :: gravit_in
-
-      gravit = gravit_in
-
-   end subroutine update_dry_static_energy_init
-
-   !> \section arg_table_update_dry_static_energy_run  Argument Table
-   !! \htmlinclude update_dry_static_energy_run.html
-   subroutine update_dry_static_energy_run(nz, temp, zm, phis, st_energy,     &
-        cpair, errcode, errmsg)
-
-      ! Dummy arguments
-      integer,            intent(in)  :: nz             ! Num vertical  layers
-      real(kind_phys),    intent(in)  :: temp(:,:)      ! air temperature
-      real(kind_phys),    intent(in)  :: zm(:,:)        ! geopotential height
-      real(kind_phys),    intent(in)  :: phis(:)        ! surface geopotential
-      real(kind_phys),    intent(out) :: st_energy(:,:) ! dry static energy
-      real(kind_phys),    intent(in)  :: cpair          ! specific heat, dry air
-      integer,            intent(out) :: errcode
-      character(len=512), intent(out) :: errmsg
-
-      ! Local variable
-      integer :: klev
-
-      errcode = 0
-      errmsg = ''
-
-      do klev = 1, nz
-         st_energy(:, klev) = (temp(:, klev) * cpair) +                       &
-              (gravit * zm(:, klev)) + phis(:)
-      end do
-
-   end subroutine update_dry_static_energy_run
 
 end module physics_tendency_updaters
---
+++ utilities/static_energy.F90
@@ -0,0 +1,54 @@
+module static_energy
+
+  use ccpp_kinds, only: kind_phys
+
+  implicit none
+  private
+
+  public :: update_dry_static_energy_init
+  public :: update_dry_static_energy_run
+
+  ! Private module variables
+
+  real(kind_phys) :: gravit = -HUGE(1.0_kind_phys)
+
+CONTAINS
+
+   !> \section arg_table_update_dry_static_energy_init  Argument Table
+   !! \htmlinclude update_dry_static_energy_init.html
+   subroutine update_dry_static_energy_init(gravit_in)
+      real(kind_phys),    intent(in)    :: gravit_in
+
+      gravit = gravit_in
+
+   end subroutine update_dry_static_energy_init
+
+   !> \section arg_table_update_dry_static_energy_run  Argument Table
+   !! \htmlinclude update_dry_static_energy_run.html
+   subroutine update_dry_static_energy_run(nz, temp, zm, phis, st_energy,     &
+        cpair, errcode, errmsg)
+
+      ! Dummy arguments
+      integer,            intent(in)  :: nz             ! Num vertical  layers
+      real(kind_phys),    intent(in)  :: temp(:,:)      ! air temperature
+      real(kind_phys),    intent(in)  :: zm(:,:)        ! geopotential height
+      real(kind_phys),    intent(in)  :: phis(:)        ! surface geopotential
+      real(kind_phys),    intent(out) :: st_energy(:,:) ! dry static energy
+      real(kind_phys),    intent(in)  :: cpair          ! specific heat, dry air
+      integer,            intent(out) :: errcode
+      character(len=512), intent(out) :: errmsg
+
+      ! Local variable
+      integer :: klev
+
+      errcode = 0
+      errmsg = ''
+
+      do klev = 1, nz
+         st_energy(:, klev) = (temp(:, klev) * cpair) +                       &
+              (gravit * zm(:, klev)) + phis(:)
+      end do
+
+   end subroutine update_dry_static_energy_run
+
+end module physics_tendency_updaters
