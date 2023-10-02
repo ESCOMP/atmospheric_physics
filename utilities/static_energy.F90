6fd70dd4a544d233c8b8fdf7e838ca9992568baf
Jesse Nusbaumer <nusbaume@ucar.edu>
2023-10-02 10:00:27 -0600

    Update static_energy scheme to allow its use in CAM.

--- utilities/static_energy.F90
+++ utilities/static_energy.F90
@@ -5,31 +5,18 @@ module static_energy
   implicit none
   private
 
-  public :: update_dry_static_energy_init
   public :: update_dry_static_energy_run
-
-  ! Private module variables
-
-  real(kind_phys) :: gravit = -HUGE(1.0_kind_phys)
 
 CONTAINS
 
-   !> \section arg_table_update_dry_static_energy_init  Argument Table
-   !! \htmlinclude update_dry_static_energy_init.html
-   subroutine update_dry_static_energy_init(gravit_in)
-      real(kind_phys),    intent(in)    :: gravit_in
-
-      gravit = gravit_in
-
-   end subroutine update_dry_static_energy_init
-
    !> \section arg_table_update_dry_static_energy_run  Argument Table
    !! \htmlinclude update_dry_static_energy_run.html
-   subroutine update_dry_static_energy_run(nz, temp, zm, phis, st_energy,     &
-        cpair, errcode, errmsg)
+   subroutine update_dry_static_energy_run(nz, gravit, temp, zm, phis,        &
+        st_energy, cpair, errcode, errmsg)
 
       ! Dummy arguments
-      integer,            intent(in)  :: nz             ! Num vertical  layers
+      integer,            intent(in)  :: nz             ! Num vertical layers
+      real(kind_phys),    intent(in)  :: gravit         ! gravitational acceleration
       real(kind_phys),    intent(in)  :: temp(:,:)      ! air temperature
       real(kind_phys),    intent(in)  :: zm(:,:)        ! geopotential height
       real(kind_phys),    intent(in)  :: phis(:)        ! surface geopotential
