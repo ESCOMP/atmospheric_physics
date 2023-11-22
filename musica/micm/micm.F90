b3b8396eb50e9f6579fb9b7240f397f8970a0d09
Jiwon Gim <jiwongim@ucar.edu>
2023-11-21 17:57:12 -0700

    set up error code for creating solver

--- musica/micm/micm.F90
+++ musica/micm/micm.F90
@@ -3,73 +3,75 @@ module micm_wrapper
 
    use iso_c_binding
    use micm_core
-    ! use ccpp_kinds, only:  kind_phys  !TODO(jiwon) - do we need this?
+   !  use ccpp_kinds, only:  kind_phys  !TODO(jiwon) - do we need this?
  
    implicit none
  
    private
    public :: micm_init, micm_run, micm_final
-   
-   type(micm_t), pointer :: micm_ptr
-   
-   ! type :: micm_wrapper_t
-   !    private
-   !    type(micm_t),     pointer :: micm_
-   ! ! contains
-   ! !    final :: finalize
-   ! end type micm_wrapper_t
 
-   ! type(micm_wrapper_t), allocatable :: micm_wrappers
+   type(micm_t), pointer :: micm_ptr
 
  contains
  
    !> \section arg_table_micm_init Argument Table
    !! \htmlinclude micm_init.html
-   subroutine micm_init(config_path, errmsg, errflg)
+   subroutine micm_init(config_path, iulog, errcode, errmsg)
       ! Initializes MICM objects by creating solvers from configure files
 
       character(len=*), intent(in)     :: config_path
+      integer, intent(in)              :: iulog
+      integer(c_size_t), intent(out)   :: errcode
       character(len=512), intent(out)  :: errmsg
-      integer(c_size_t), intent(out)   :: errflg
- 
+
+      errcode = 0
       errmsg = ''
-      errflg = 0
-      
+
       ! Constructs MICM object
       allocate(micm_ptr)
       micm_ptr = micm_t(config_path)
-      
-      write(*,*) "  * [MICM wrapper] Created MICM solver "
 
       ! Creates solver
-      errflg = micm_ptr%create_solver()  ! TODO(jiwon)
+      errcode = micm_ptr%create_solver()
 
-      ! allocate(micm_wrappers)
-      ! micm_wrappers%micm_ = micm_t(config_path)
+      if (errcode /= 0) then
+         errmsg = "INIT MICM: FATAL: Failed in creating MICM solver because parsing configuration files failed. &
+                  Please look over at the other file."  !TODO(jiwon) - which file?
+      else 
+         write(iulog,*) "INIT MICM: INFO: Created MICM solver"
+      endif
 
    end subroutine micm_init
 
    !> \section arg_table_micm_run Argument Table
    !! \htmlinclude micm_run.html
-   subroutine micm_run(temperature, pressure, time_step, concentrations, num_concentrations, errmsg, errflg)
-      real(c_double), intent(in) :: temperature
-      real(c_double), intent(in) :: pressure
-      real(c_double), intent(in) :: time_step
-      real(c_double), dimension(*), intent(inout) :: concentrations
-      integer(c_size_t), intent(in) :: num_concentrations
-      character(len=512), intent(out) :: errmsg
-      integer(c_size_t), intent(out) :: errflg
+   subroutine micm_run(temperature, pressure, time_step, concentrations, num_concentrations, iulog, errcode, errmsg)
+      real(c_double), intent(in)                   :: temperature
+      real(c_double), intent(in)                   :: pressure
+      real(c_double), intent(in)                   :: time_step
+      real(c_double), dimension(*), intent(inout)  :: concentrations
+      integer(c_size_t), intent(in)                :: num_concentrations
+      integer, intent(in)                          :: iulog
+      integer(c_size_t), intent(out)               :: errcode
+      character(len=512), intent(out)              :: errmsg
 
+      errcode = 0
       errmsg = ''
-      errflg = 0
 
-      write(*,*) "  * [MICM wrapper] Running MICM solver... "
-      call micm_ptr%solve(temperature, pressure, time_step, concentrations, num_concentrations)  ! TODO(jiwon): handling error
+      write(iulog,*) "RUN MICM: INFO: Running MICM solver..."
+      call micm_ptr%solve(temperature, pressure, time_step, concentrations, num_concentrations)
 
    end subroutine micm_run
  
-   subroutine micm_final()
-      write(*,*) "  * [MICM wrapper] Deallocating MICM object... "
+   subroutine micm_final(iulog, errcode, errmsg)
+      integer, intent(in)              :: iulog
+      integer(c_size_t), intent(out)   :: errcode
+      character(len=512), intent(out)  :: errmsg
+
+      errcode = 0
+      errmsg = ''
+      
+      write(iulog,*) "FINAL MICM: INFO: Deallocating MICM object..."
       deallocate(micm_ptr)
 
    end subroutine micm_final
