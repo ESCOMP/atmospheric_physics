12edfa9432ae73be2b1a440b77c99d1239422523
Jiwon Gim <jiwongim@ucar.edu>
2023-12-14 17:44:56 -0700

    add kind phy

--- musica/micm/micm.F90
+++ musica/micm/micm.F90
@@ -47,34 +47,44 @@ module micm
    !> \section arg_table_micm_run Argument Table
    !! \htmlinclude micm_run.html
    subroutine micm_run(temperature, pressure, time_step, concentrations, num_concentrations, iulog, errcode, errmsg)
-      real(kind_phys), intent(in)                                    :: temperature
-      real(kind_phys), intent(in)                                    :: pressure
-      real(kind_phys), intent(in)                                    :: time_step
-      real(kind_phys), dimension(:,:,:), allocatable, intent(inout)  :: concentrations      
+      real(kind_phys), dimension(:,:), intent(in)        :: temperature
+      real(kind_phys), dimension(:,:), intent(in)        :: pressure
+      real(kind_phys), intent(in)                        :: time_step
+      real(kind_phys), dimension(:,:,:), intent(inout)   :: concentrations      
+      integer, intent(in)                                :: num_concentrations
+      integer, intent(in)                                :: iulog
+      integer, intent(out)                               :: errcode
+      character(len=512), intent(out)                    :: errmsg
 
-      real(c_double)                                                 :: c_temperature
-      real(c_double)                                                 :: c_pressure
-      real(c_double)                                                 :: c_time_step
-      real(c_double), dimension(:,:,:), allocatable                  :: c_concentrations
-   
-      integer, intent(in)                                            :: num_concentrations
-      integer, intent(in)                                            :: iulog
-      integer, intent(out)                                           :: errcode
-      character(len=512), intent(out)                                :: errmsg
-      
-      real(kind_phys) :: var_phys
-      real(c_double)  :: var_doub
-      
+      ! local variables
+      real(c_double), dimension(shape(temperature))      :: c_temperature
+      real(c_double), dimension(shape(pressure))         :: c_pressure
+      real(c_double)                                     :: c_time_step
+      real(c_double), dimension(shape(concentrations))   :: c_concentrations
+      integer                                            :: horizontal_loop_extent
+      integer                                            :: vertical_layer_dimension
+      integer                                            :: i_column
+      integer                                            :: i_layer
+
       c_temperature = real(temperature, c_double)
       c_pressure = real(pressure, c_double)
       c_time_step = real(time_step, c_double)
       c_concentrations = real(concentrations, c_double)
+      horizontal_loop_extent = size(concentrations, 1)
+      vertical_layer_dimension  = size(concentrations, 2)
 
       errcode = 0
       errmsg = ''
 
       write(iulog,*) "RUN MICM: INFO: Running MICM solver..."
-      call micm_ptr%solve(c_temperature, c_pressure, c_time_step, c_concentrations, num_concentrations)
+      do i_column = 1, horizontal_loop_extent
+         do i_layer = 1, vertical_layer_dimension
+            call micm_ptr%solve(c_temperature(i_column, i_layer), c_pressure(i_column, i_layer), c_time_step, &
+                                c_concentrations(i_column, i_layer, :), num_concentrations)
+         end do
+      end do 
+
+      concentrations = real(c_concentrations, kind_phys)
 
    end subroutine micm_run
 
