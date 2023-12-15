b770380684f19f73d7ba5dd22c38eeda85d1ba87
Jiwon Gim <jiwongim@ucar.edu>
2023-12-15 12:32:04 -0700

    fixed the error for deferred shape array

--- musica/micm/micm.F90
+++ musica/micm/micm.F90
@@ -47,29 +47,33 @@ module micm
    !> \section arg_table_micm_run Argument Table
    !! \htmlinclude micm_run.html
    subroutine micm_run(temperature, pressure, time_step, num_concentrations, concentrations, iulog, errcode, errmsg)
-      real(kind_phys), dimension(:,:), intent(in)        :: temperature
-      real(kind_phys), dimension(:,:), intent(in)        :: pressure
-      real(kind_phys), intent(in)                        :: time_step
-      integer, intent(in)                                :: num_concentrations
-      real(kind_phys), dimension(:,:,:), intent(inout)   :: concentrations      
-      integer, intent(in)                                :: iulog
-      integer, intent(out)                               :: errcode
-      character(len=512), intent(out)                    :: errmsg
+      real(kind_phys), dimension(:,:), intent(in)       :: temperature
+      real(kind_phys), dimension(:,:), intent(in)       :: pressure
+      real(kind_phys), intent(in)                       :: time_step
+      integer, intent(in)                               :: num_concentrations
+      real(kind_phys), dimension(:,:,:), intent(inout)  :: concentrations      
+      integer, intent(in)                               :: iulog
+      integer, intent(out)                              :: errcode
+      character(len=512), intent(out)                   :: errmsg
 
       ! local variables
-      real(c_double), dimension(shape(temperature))      :: c_temperature
-      real(c_double), dimension(shape(pressure))         :: c_pressure
-      real(c_double)                                     :: c_time_step
-      real(c_double), dimension(shape(concentrations))   :: c_concentrations
-      integer                                            :: horizontal_loop_extent, vertical_layer_dimension
-      integer                                            :: i_column, i_layer
+      real(c_double), dimension(size(temperature, dim=1), &
+                                size(temperature, dim=2))     :: c_temperature
+      real(c_double), dimension(size(pressure, dim=1), &
+                                size(pressure, dim=2))        :: c_pressure
+      real(c_double)                                          :: c_time_step
+      real(c_double), dimension(size(concentrations, dim=1), &
+                                size(concentrations, dim=2), &
+                                size(concentrations, dim=3))  :: c_concentrations
+      integer                                                 :: horizontal_loop_extent, vertical_layer_dimension
+      integer                                                 :: i_column, i_layer
 
       c_temperature = real(temperature, c_double)
       c_pressure = real(pressure, c_double)
       c_time_step = real(time_step, c_double)
       c_concentrations = real(concentrations, c_double)
-      horizontal_loop_extent = size(concentrations, 1)
-      vertical_layer_dimension  = size(concentrations, 2)
+      horizontal_loop_extent = size(concentrations, dim=1)
+      vertical_layer_dimension = size(concentrations, dim=2)
 
       errcode = 0
       errmsg = ''
@@ -78,7 +82,7 @@ module micm
       do i_column = 1, horizontal_loop_extent
          do i_layer = 1, vertical_layer_dimension
             call micm_ptr%solve(c_temperature(i_column, i_layer), c_pressure(i_column, i_layer), c_time_step, &
-                                c_concentrations(i_column, i_layer, :), num_concentrations)
+                                num_concentrations, c_concentrations(i_column, i_layer, :))
          end do
       end do 
 
