11c551dffb1eb2fa055358b68314db2f792afe49
Jiwon Gim <jiwongim@ucar.edu>
2024-08-23 21:43:57 -0600

    add util unit test

--- test/musica/micm/test_micm_util.F90
+++ test/musica/micm/test_micm_util.F90
@@ -4,8 +4,10 @@ program test_micm_util
   use micm_util
   use ccpp_kinds,   only: kind_phys
 
+  implicit none
+
 #define ASSERT(x) if (.not.(x)) then; write(*,*) "Assertion failed[", __FILE__, ":", __LINE__, "]: x"; stop 1; endif
-#define ASSERT_NEAR( a, b, abs_error ) if( abs(a - b) > abs_error) then; write(*,*) "Assertion failed[", __FILE__, ":", __LINE__, "]: x"; stop 1; endif
+#define ASSERT_NEAR( a, b, abs_error ) if( (abs(a - b) >= abs_error) .and. (abs(a - b) /= 0.0) ) then; write(*,*) "Assertion failed[", __FILE__, ":", __LINE__, "]: a, b"; stop 1; endif
 
   call test_reshape()
   call test_unit_conversion()
@@ -112,48 +114,56 @@ contains
   end subroutine test_reshape
 
   subroutine test_unit_conversion()
-    use iso_c_binding, only: c_double
+    use ccpp_kinds,    only: kind_phys
 
-    integer, parameter :: NUM_GRID_CELLS = 4
-    integer, parameter :: NUM_SPECIES = 5
-    real(c_double)     :: dry_air_density(NUM_GRID_CELLS)     ! kg m-3
-    real(c_double)     :: molar_mass_arr(NUM_SPECIES)         ! kg mol-1
-    real(c_double)     :: constituents(NUM_SPECIES)           ! kg kg-1
-    real(c_double)     :: converted(NUM_SPECIES)              ! mol m-3
+    integer, parameter                                                     :: NUM_COLUMNS = 2
+    integer, parameter                                                     :: NUM_LAYERS = 2
+    integer, parameter                                                     :: NUM_SPECIES = 4
+    real(kind_phys), target, dimension(NUM_COLUMNS,NUM_LAYERS)             :: dry_air_density   ! kg m-3
+    real(kind_phys), target, dimension(NUM_SPECIES)                        :: molar_mass_arr
+    real(kind_phys), target, dimension(NUM_COLUMNS,NUM_LAYERS,NUM_SPECIES) :: constituents ! kg kg-1
+    real(kind_phys), target, dimension(NUM_COLUMNS,NUM_LAYERS,NUM_SPECIES) :: ccpp_constituents ! mol m-3
+    real(kind_phys), target, dimension(NUM_COLUMNS,NUM_LAYERS,NUM_SPECIES) :: micm_constituents ! mol m-3
+    integer                                                                :: i_column, i_layer, i_elem
+    real                                                                   :: abs_error = 1e-3
+    dry_air_density(:,1) = (/ 3.5_kind_phys, 4.5_kind_phys /)
+    dry_air_density(:,2) = (/ 5.5_kind_phys, 6.5_kind_phys /)
+    molar_mass_arr(:) = (/ 200._kind_phys, 200._kind_phys, 200._kind_phys, 200._kind_phys /)
+    constituents(1,1,:) = (/ 0.1_kind_phys, 0.2_kind_phys, 0.3_kind_phys, 0.4_kind_phys /)
+    constituents(1,2,:) = (/ 0.41_kind_phys, 0.42_kind_phys, 0.43_kind_phys, 0.44_kind_phys /)
+    constituents(2,1,:) = (/ 0.21_kind_phys, 0.22_kind_phys, 0.23_kind_phys, 0.24_kind_phys /)
+    constituents(2,2,:) = (/ 0.31_kind_phys, 0.32_kind_phys, 0.33_kind_phys, 0.34_kind_phys /)
 
-    dry_air_density(:) = (/ 3.5, 4.5, 5.5, 6.5 /)
-    molar_mass_arr = (/ 200., 200., 200., 200., 200 /)
-    constituents(:) = (/ 0.1, 0.2, 0.3, 0.2, 0.1 /)
+    ccpp_constituents(1,1,:) = (/ 0.1_kind_phys, 0.2_kind_phys, 0.3_kind_phys, 0.4_kind_phys /)
+    ccpp_constituents(1,2,:) = (/ 0.41_kind_phys, 0.42_kind_phys, 0.43_kind_phys, 0.44_kind_phys /)
+    ccpp_constituents(2,1,:) = (/ 0.21_kind_phys, 0.22_kind_phys, 0.23_kind_phys, 0.24_kind_phys /)
+    ccpp_constituents(2,2,:) = (/ 0.31_kind_phys, 0.32_kind_phys, 0.33_kind_phys, 0.34_kind_phys /)
 
-    converted_constituents(:)
+    micm_constituents(1,1,:) = (/ 1.750E-003_kind_phys,   3.500E-003_kind_phys,   5.250E-003_kind_phys,   7.000E-003_kind_phys /)
+    micm_constituents(1,2,:) = (/ 1.127E-002_kind_phys,   1.155E-002_kind_phys,   1.182E-002_kind_phys,   1.210E-002_kind_phys /)
+    micm_constituents(2,1,:) = (/ 4.725E-003_kind_phys,   4.949E-003_kind_phys,   5.175E-003_kind_phys,   5.400E-003_kind_phys /)
+    micm_constituents(2,2,:) = (/ 1.007E-002_kind_phys,   1.040E-002_kind_phys,   1.072E-002_kind_phys,   1.104E-002_kind_phys /)
+
     call convert_to_mol_per_cubic_meter(dry_air_density, molar_mass_arr, constituents)
 
+    do i_column = 1, NUM_COLUMNS
+      do i_layer = 1, NUM_LAYERS
+        do i_elem = 1, NUM_SPECIES
+          write(*,*) "c", constituents(i_column, i_layer, i_elem) - micm_constituents(i_column, i_layer, i_elem)
+          ASSERT_NEAR(constituents(i_column, i_layer, i_elem), micm_constituents(i_column, i_layer, i_elem), abs_error)
+        end do
+      end do
+    end do
+
+    call convert_to_mass_mixing_ratio(dry_air_density, molar_mass_arr, constituents)
+
+    do i_column = 1, NUM_COLUMNS
+      do i_layer = 1, NUM_LAYERS
+        do i_elem = 1, NUM_SPECIES
+          ASSERT_NEAR(constituents(i_column, i_layer, i_elem), ccpp_constituents(i_column, i_layer, i_elem), abs_error)
+        end do
+      end do
+    end do
   end subroutine test_unit_conversion
-
-
-
-
-
-
-  subroutine convert_to_mol_per_cubic_meter(dry_air_density, molar_mass_arr, constituents)
-    use iso_c_binding, only: c_double
-
-    integer, parameter      :: NUM_GRID_CELLS = 4
-    real(c_double), intent(in)    :: dry_air_density(NUM_GRID_CELLS) ! kg m-3
-    real(c_double), intent(in)    :: molar_mass_arr(NUM_GRID_CELLS)  ! kg mol-1
-    real(c_double), intent(inout) :: constituents(:)    ! in: kg kg-1 | out: mol m-3
-
-
-    integer        :: i_elem
-    real(c_double) :: val
-
-    dry_air_density(:,1) = (/ 3.5, 4.5, 5.5, 6.5 /)
-    
-  ! Convert MICM unit to CAM-SIMA unit (mol m-3  ->  kg kg-1)
-  ! call convert_to_mass_mixing_ratio(dry_air_density, molar_mass_arr, constituents)
-
-  ! ! Convert CAM-SIMA unit to MICM unit (kg kg-1  ->  mol m-3)
-  ! call convert_to_mol_per_cubic_meter(dry_air_density, molar_mass_arr, constituents)
-
 
 end program test_micm_util
