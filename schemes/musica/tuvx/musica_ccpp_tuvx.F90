862ac09096bda64fd3617364bc326203daa437b7
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2024-10-30 10:56:59 -0600

    Update surface albedo in TUV-x prior to calculating rate constants  (#141)
    Originator(s): @mattldawson, @boulderdaze
    
    Summary (include the keyword ['closes', 'fixes', 'resolves'] and issue
    number):
    - Updates TUV-x wrapper, tests, and configuration data to allow setting
    the wavelength grid from the host model. This implementation assumes
    that there will be a standard-named variable that defines the edges of
    the wavelength grid to use for photolysis rate constant calculations
    that is defined by the host model.
    - Updates the profile of surface albedos in TUV-x for each column prior
    to calculating photolysis rate constants.
    - Closes #96 and #123
    
    Describe any changes made to the namelist: N/A
    
    List all files eliminated and why: N/A
    
    List all files added and what they do:
    - A       schemes/musica/tuvx/musica_ccpp_tuvx_surface_albedo.F90
    - A       schemes/musica/tuvx/musica_ccpp_tuvx_util.F90
    - A       schemes/musica/tuvx/musica_ccpp_tuvx_wavelength_grid.F90
    - A       test/musica/tuvx/test_tuvx_surface_albedo.F90
    - A       test/musica/tuvx/test_tuvx_wavelength_grid.F90
    
    List all existing files that have been modified, and describe the
    changes:
    - M       schemes/musica/musica_ccpp.F90
    - M       schemes/musica/musica_ccpp.meta
    - M       schemes/musica/tuvx/musica_ccpp_tuvx.F90
    - M       test/musica/micm/test_micm_util.F90
    - M       test/musica/test_musica_api.F90
    - M       test/musica/tuvx/CMakeLists.txt
    - M       test/musica/tuvx/configs/ts1_tsmlt.json
    - M       test/musica/tuvx/test_tuvx_height_grid.F90
    - M       test/musica/tuvx/test_tuvx_temperature.F90
    
    List any test failures: N/A
    
    Is this a science-changing update? New physics package, algorithm
    change, tuning changes, etc? No
    
    ---------
    
    Co-authored-by: Matt Dawson <mattldawson@gmail.com>

--- schemes/musica/tuvx/musica_ccpp_tuvx.F90
+++ schemes/musica/tuvx/musica_ccpp_tuvx.F90
@@ -1,6 +1,6 @@
 module musica_ccpp_tuvx
 
-  ! Note: "tuvx_t" is included in an external pre-built tuvx library that the host
+  ! Note: "tuvx_t" is included in an external pre-built TUV-x library that the host
   ! model is responsible for linking to during compilation
   use musica_tuvx,          only: tuvx_t, grid_t, profile_t
   use musica_ccpp_util,     only: has_error_occurred
@@ -12,23 +12,32 @@ module musica_ccpp_tuvx
 
   public :: tuvx_init, tuvx_run, tuvx_final
 
-  type(tuvx_t),    pointer :: tuvx => null( )
-  type(grid_t),    pointer :: height_grid => null( )
-  type(profile_t), pointer :: temperature_profile => null( )
+  type(tuvx_t),    pointer :: tuvx => null()
+  type(grid_t),    pointer :: height_grid => null()
+  type(grid_t),    pointer :: wavelength_grid => null()
+  type(profile_t), pointer :: temperature_profile => null()
+  type(profile_t), pointer :: surface_albedo_profile => null()
 
 contains
 
-  !> Intitialize TUV-x
+  !> Initializes TUV-x
   subroutine tuvx_init(vertical_layer_dimension, vertical_interface_dimension, &
-                       errmsg, errcode)
+                       wavelength_grid_interfaces, errmsg, errcode)
     use musica_tuvx, only: grid_map_t, profile_map_t, radiator_map_t
     use musica_util, only: error_t
-    use musica_ccpp_tuvx_height_grid, only: create_height_grid, &
-                                            height_grid_label, height_grid_unit
-    use musica_ccpp_tuvx_temperature, only: create_temperature_profile, &
-                                            temperature_label, temperature_unit
-    integer,            intent(in)  :: vertical_layer_dimension     ! (count)
-    integer,            intent(in)  :: vertical_interface_dimension ! (count)
+    use musica_ccpp_tuvx_util, only: tuvx_deallocate
+    use musica_ccpp_tuvx_height_grid, &
+      only: create_height_grid, height_grid_label, height_grid_unit
+    use musica_ccpp_tuvx_wavelength_grid, &
+      only: create_wavelength_grid, wavelength_grid_label, wavelength_grid_unit
+    use musica_ccpp_tuvx_temperature, &
+      only: create_temperature_profile, temperature_label, temperature_unit
+    use musica_ccpp_tuvx_surface_albedo, &
+      only: create_surface_albedo_profile, surface_albedo_label, surface_albedo_unit
+
+    integer,            intent(in)  :: vertical_layer_dimension      ! (count)
+    integer,            intent(in)  :: vertical_interface_dimension  ! (count)
+    real(kind_phys),    intent(in)  :: wavelength_grid_interfaces(:) ! m
     character(len=512), intent(out) :: errmsg
     integer,            intent(out) :: errcode
 
@@ -50,71 +59,79 @@ contains
 
     call grids%add( height_grid, error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( grids )
-      deallocate( height_grid )
-      height_grid => null()
+      call tuvx_deallocate( grids, null(), null(), null(), height_grid, null(), &
+                            null(), null() )
+      return
+    end if
+
+    wavelength_grid => create_wavelength_grid( wavelength_grid_interfaces, &
+                                               errmsg, errcode )
+    if (errcode /= 0) then
+      call tuvx_deallocate( grids, null(), null(), null(), height_grid, null(), &
+                            null(), null() )
+      return
+    endif
+
+    call grids%add( wavelength_grid, error )
+    if (has_error_occurred( error, errmsg, errcode )) then
+      call tuvx_deallocate( grids, null(), null(), null(), height_grid, &
+                            wavelength_grid, null(), null() )
       return
     end if
 
     profiles => profile_map_t( error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( grids )
-      deallocate( height_grid )
-      height_grid => null()
+      call tuvx_deallocate( grids, null(), null(), null(), height_grid, &
+                            wavelength_grid, null(), null() )
       return
     end if
 
     temperature_profile => create_temperature_profile( height_grid, errmsg, errcode )
     if (errcode /= 0) then
-      deallocate( grids )
-      deallocate( height_grid )
-      height_grid => null()
-      deallocate( profiles )
+      call tuvx_deallocate( grids, profiles, null(), null(), height_grid, &
+                            wavelength_grid, null(), null() )
       return
     endif
 
     call profiles%add( temperature_profile, error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( grids )
-      deallocate( height_grid )
-      height_grid => null()
-      deallocate( profiles )
-      deallocate( temperature_profile )
-      temperature_profile => null()
+      call tuvx_deallocate( grids, profiles, null(), null(), height_grid, &
+                            wavelength_grid, temperature_profile, null() )
+      return
+    end if
+
+    surface_albedo_profile => create_surface_albedo_profile( wavelength_grid, &
+                                                             errmsg, errcode )
+    if (errcode /= 0) then
+      call tuvx_deallocate( grids, profiles, null(), null(), height_grid, &
+                            wavelength_grid, temperature_profile, null() )
+      return
+    endif
+
+    call profiles%add( surface_albedo_profile, error )
+    if (has_error_occurred( error, errmsg, errcode )) then
+      call tuvx_deallocate( grids, profiles, null(), null(), height_grid, &
+            wavelength_grid, temperature_profile, surface_albedo_profile )
       return
     end if
 
     radiators => radiator_map_t( error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( grids )
-      deallocate( height_grid )
-      height_grid => null()
-      deallocate( profiles )
-      deallocate( temperature_profile )
-      temperature_profile => null()
+      call tuvx_deallocate( grids, profiles, null(), null(), height_grid, &
+            wavelength_grid, temperature_profile, surface_albedo_profile )
       return
     end if
 
     tuvx => tuvx_t( filename_of_tuvx_configuration, grids, profiles, &
                     radiators, error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( grids )
-      deallocate( height_grid )
-      height_grid => null()
-      deallocate( profiles )
-      deallocate( temperature_profile )
-      temperature_profile => null()
-      deallocate( radiators )
+      call tuvx_deallocate( grids, profiles, radiators, null(), height_grid, &
+            wavelength_grid, temperature_profile, surface_albedo_profile )
       return
     end if
 
-    deallocate( grids )
-    deallocate( height_grid )
-    height_grid => null()
-    deallocate( profiles )
-    deallocate( temperature_profile )
-    temperature_profile => null()
-    deallocate( radiators )
+    call tuvx_deallocate( grids, profiles, radiators, null(), height_grid, &
+          wavelength_grid, temperature_profile, surface_albedo_profile )
 
     grids => tuvx%get_grids( error )
     if (has_error_occurred( error, errmsg, errcode )) then
@@ -125,30 +142,42 @@ contains
 
     height_grid => grids%get( height_grid_label, height_grid_unit, error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( tuvx )
-      tuvx => null()
-      deallocate( grids )
+      call tuvx_deallocate( grids, null(), null(), tuvx, null(), null(), &
+                            null(), null() )
       return
     end if
 
-    deallocate( grids )
+    wavelength_grid => grids%get( wavelength_grid_label, wavelength_grid_unit, &
+                                  error )
+    if (has_error_occurred( error, errmsg, errcode )) then
+      call tuvx_deallocate( grids, null(), null(), tuvx, height_grid, null(), &
+                            null(), null() )
+      return
+    end if
 
     profiles => tuvx%get_profiles( error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( tuvx )
-      tuvx => null()
+      call tuvx_deallocate( grids, null(), null(), tuvx, height_grid, &
+            wavelength_grid, null(), null() )
       return
     end if
 
     temperature_profile => profiles%get( temperature_label, temperature_unit, error )
     if (has_error_occurred( error, errmsg, errcode )) then
-      deallocate( tuvx )
-      tuvx => null()
-      deallocate( profiles )
+      call tuvx_deallocate( grids, profiles, null(), tuvx, height_grid, &
+            wavelength_grid, null(), null() )
       return
     end if
 
-    deallocate( profiles )
+    surface_albedo_profile => profiles%get( surface_albedo_label, surface_albedo_unit, error )
+    if (has_error_occurred( error, errmsg, errcode )) then
+      call tuvx_deallocate( grids, profiles, null(), tuvx, height_grid, &
+            wavelength_grid, temperature_profile, null() )
+      return
+    end if
+
+    call tuvx_deallocate( grids, profiles, null(), null(), null(), null(), &
+                          null(), null() )
 
   end subroutine tuvx_init
 
@@ -157,11 +186,13 @@ contains
                       geopotential_height_wrt_surface_at_midpoint,  &
                       geopotential_height_wrt_surface_at_interface, &
                       surface_temperature, surface_geopotential,    &
+                      surface_albedo,                               &
                       standard_gravitational_acceleration,          &
                       photolysis_rate_constants, errmsg, errcode)
-    use musica_util,                  only: error_t
-    use musica_ccpp_tuvx_height_grid, only: set_height_grid_values, calculate_heights
-    use musica_ccpp_tuvx_temperature, only: set_temperature_values
+    use musica_util,                     only: error_t
+    use musica_ccpp_tuvx_height_grid,    only: set_height_grid_values, calculate_heights
+    use musica_ccpp_tuvx_temperature,    only: set_temperature_values
+    use musica_ccpp_tuvx_surface_albedo, only: set_surface_albedo_values
 
     real(kind_phys),    intent(in)  :: temperature(:,:)                                  ! K (column, layer)
     real(kind_phys),    intent(in)  :: dry_air_density(:,:)                              ! kg m-3 (column, layer)
@@ -169,6 +200,7 @@ contains
     real(kind_phys),    intent(in)  :: geopotential_height_wrt_surface_at_interface(:,:) ! m (column, interface)
     real(kind_phys),    intent(in)  :: surface_temperature(:)                            ! K
     real(kind_phys),    intent(in)  :: surface_geopotential(:)                           ! m2 s-2
+    real(kind_phys),    intent(in)  :: surface_albedo                                    ! unitless
     real(kind_phys),    intent(in)  :: standard_gravitational_acceleration               ! m s-2
     ! temporarily set to Chapman mechanism and 1 dimension
     ! until mapping between MICM and TUV-x is implemented
@@ -199,12 +231,16 @@ contains
       if (errcode /= 0) return
     end do
 
+    ! surface albedo with respect to direct UV/visible radiation
+    call set_surface_albedo_values( surface_albedo_profile, surface_albedo, errmsg, errcode )
+    if (errcode /= 0) return
+
     ! stand-in until actual photolysis rate constants are calculated
     photolysis_rate_constants(:) = 1.0e-6_kind_phys
 
   end subroutine tuvx_run
 
-  !> Finalize tuvx
+  !> Finalizes TUV-x
   subroutine tuvx_final(errmsg, errcode)
     character(len=512), intent(out) :: errmsg
     integer,            intent(out) :: errcode
@@ -217,9 +253,19 @@ contains
       height_grid => null()
     end if
 
+    if (associated( wavelength_grid )) then
+      deallocate( wavelength_grid )
+      wavelength_grid => null()
+    end if
+
     if (associated( temperature_profile )) then
       deallocate( temperature_profile )
       temperature_profile => null()
+    end if
+
+    if (associated( surface_albedo_profile )) then
+      deallocate( surface_albedo_profile )
+      surface_albedo_profile => null()
     end if
 
     if (associated( tuvx )) then
--- test/musica/test_musica_api.F90
+++ test/musica/test_musica_api.F90
@@ -16,44 +16,151 @@ contains
     use ccpp_constituent_prop_mod, only: ccpp_constituent_prop_ptr_t
     use ccpp_constituent_prop_mod, only: ccpp_constituent_properties_t
 
-    implicit none
-
     integer, parameter                                             :: NUM_SPECIES = 4
     integer, parameter                                             :: NUM_COLUMNS = 2
     integer, parameter                                             :: NUM_LAYERS = 2
-    integer                                                        :: solver_type
+    integer, parameter                                             :: NUM_WAVELENGTH_BINS = 102
+    integer                                                        :: num_grid_cells = NUM_COLUMNS * NUM_LAYERS
+    integer                                                        :: solver_type = Rosenbrock
     integer                                                        :: errcode
     character(len=512)                                             :: errmsg
-    real(kind_phys)                                                :: time_step                                    ! s
+    real(kind_phys)                                                :: time_step = 60._kind_phys                    ! s
+    real(kind_phys), dimension(NUM_WAVELENGTH_BINS+1)              :: photolysis_wavelength_grid_interfaces        ! m
     real(kind_phys), dimension(NUM_COLUMNS,NUM_LAYERS)             :: geopotential_height_wrt_surface_at_midpoint  ! m
     real(kind_phys), dimension(NUM_COLUMNS,NUM_LAYERS+1)           :: geopotential_height_wrt_surface_at_interface ! m
     real(kind_phys), dimension(NUM_COLUMNS)                        :: surface_temperature                          ! K
     real(kind_phys), dimension(NUM_COLUMNS)                        :: surface_geopotential                         ! m2 s-2
+    real(kind_phys)                                                :: surface_albedo                               ! unitless
     real(kind_phys)                                                :: standard_gravitational_acceleration          ! s2 m-1
     real(kind_phys), dimension(NUM_COLUMNS,NUM_LAYERS)             :: temperature                                  ! K
     real(kind_phys), dimension(NUM_COLUMNS,NUM_LAYERS)             :: pressure                                     ! Pa
     real(kind_phys), dimension(NUM_COLUMNS,NUM_LAYERS)             :: dry_air_density                              ! kg m-3
     real(kind_phys), dimension(NUM_COLUMNS,NUM_LAYERS,NUM_SPECIES) :: constituents                                 ! kg kg-1
     type(ccpp_constituent_prop_ptr_t), allocatable                 :: constituent_props_ptr(:)
+    type(ccpp_constituent_properties_t), allocatable, target       :: constituent_props(:)
+    type(ccpp_constituent_properties_t), pointer                   :: const_prop
+    real(kind_phys)                                                :: molar_mass
+    character(len=512)                                             :: species_name, units
+    logical                                                        :: tmp_bool, is_advected
+    integer                                                        :: i
 
-    ! local variables
-    type(ccpp_constituent_properties_t), allocatable, target :: constituent_props(:)
-    type(ccpp_constituent_properties_t), pointer             :: const_prop
-    real(kind_phys)                                          :: molar_mass
-    character(len=512)                                       :: species_name, units
-    logical                                                  :: tmp_bool, is_advected
-    integer                                                  :: num_grid_cells
-    integer                                                  :: i
-
-    solver_type = Rosenbrock
-    num_grid_cells = NUM_COLUMNS * NUM_LAYERS
-    time_step = 60._kind_phys
+    ! These are the values that will be used in CAM-SIMA and correspond to the wavelength
+    ! bins used in the CAM-Chem photolysis rate constant lookup table.
+    !
+    ! We're using the actual values here because several of the TS1/TSMLT photolysis
+    ! rate constant configurations are sensitive to the wavelength grid.
+    photolysis_wavelength_grid_interfaces = (/ &
+      120.0e-9_kind_phys, &
+      121.4e-9_kind_phys, &
+      121.9e-9_kind_phys, &
+      123.5e-9_kind_phys, &
+      124.3e-9_kind_phys, &
+      125.5e-9_kind_phys, &
+      126.3e-9_kind_phys, &
+      127.1e-9_kind_phys, &
+      130.1e-9_kind_phys, &
+      131.1e-9_kind_phys, &
+      135.0e-9_kind_phys, &
+      140.0e-9_kind_phys, &
+      145.0e-9_kind_phys, &
+      150.0e-9_kind_phys, &
+      155.0e-9_kind_phys, &
+      160.0e-9_kind_phys, &
+      165.0e-9_kind_phys, &
+      168.0e-9_kind_phys, &
+      171.0e-9_kind_phys, &
+      173.0e-9_kind_phys, &
+      174.4e-9_kind_phys, &
+      175.4e-9_kind_phys, &
+      177.0e-9_kind_phys, &
+      178.6e-9_kind_phys, &
+      180.2e-9_kind_phys, &
+      181.8e-9_kind_phys, &
+      183.5e-9_kind_phys, &
+      185.2e-9_kind_phys, &
+      186.9e-9_kind_phys, &
+      188.7e-9_kind_phys, &
+      190.5e-9_kind_phys, &
+      192.3e-9_kind_phys, &
+      194.2e-9_kind_phys, &
+      196.1e-9_kind_phys, &
+      198.0e-9_kind_phys, &
+      200.0e-9_kind_phys, &
+      202.0e-9_kind_phys, &
+      204.1e-9_kind_phys, &
+      206.2e-9_kind_phys, &
+      208.0e-9_kind_phys, &
+      211.0e-9_kind_phys, &
+      214.0e-9_kind_phys, &
+      217.0e-9_kind_phys, &
+      220.0e-9_kind_phys, &
+      223.0e-9_kind_phys, &
+      226.0e-9_kind_phys, &
+      229.0e-9_kind_phys, &
+      232.0e-9_kind_phys, &
+      235.0e-9_kind_phys, &
+      238.0e-9_kind_phys, &
+      241.0e-9_kind_phys, &
+      244.0e-9_kind_phys, &
+      247.0e-9_kind_phys, &
+      250.0e-9_kind_phys, &
+      253.0e-9_kind_phys, &
+      256.0e-9_kind_phys, &
+      259.0e-9_kind_phys, &
+      263.0e-9_kind_phys, &
+      267.0e-9_kind_phys, &
+      271.0e-9_kind_phys, &
+      275.0e-9_kind_phys, &
+      279.0e-9_kind_phys, &
+      283.0e-9_kind_phys, &
+      287.0e-9_kind_phys, &
+      291.0e-9_kind_phys, &
+      295.0e-9_kind_phys, &
+      298.5e-9_kind_phys, &
+      302.5e-9_kind_phys, &
+      305.5e-9_kind_phys, &
+      308.5e-9_kind_phys, &
+      311.5e-9_kind_phys, &
+      314.5e-9_kind_phys, &
+      317.5e-9_kind_phys, &
+      322.5e-9_kind_phys, &
+      327.5e-9_kind_phys, &
+      332.5e-9_kind_phys, &
+      337.5e-9_kind_phys, &
+      342.5e-9_kind_phys, &
+      347.5e-9_kind_phys, &
+      350.0e-9_kind_phys, &
+      355.0e-9_kind_phys, &
+      360.0e-9_kind_phys, &
+      365.0e-9_kind_phys, &
+      370.0e-9_kind_phys, &
+      375.0e-9_kind_phys, &
+      380.0e-9_kind_phys, &
+      385.0e-9_kind_phys, &
+      390.0e-9_kind_phys, &
+      395.0e-9_kind_phys, &
+      400.0e-9_kind_phys, &
+      405.0e-9_kind_phys, &
+      410.0e-9_kind_phys, &
+      415.0e-9_kind_phys, &
+      420.0e-9_kind_phys, &
+      430.0e-9_kind_phys, &
+      440.0e-9_kind_phys, &
+      450.0e-9_kind_phys, &
+      500.0e-9_kind_phys, &
+      550.0e-9_kind_phys, &
+      600.0e-9_kind_phys, &
+      650.0e-9_kind_phys, &
+      700.0e-9_kind_phys, &
+      750.0e-9_kind_phys &
+    /)
     geopotential_height_wrt_surface_at_midpoint(1,:) = (/ 2000.0_kind_phys, 500.0_kind_phys /)
     geopotential_height_wrt_surface_at_midpoint(2,:) = (/ 2000.0_kind_phys, -500.0_kind_phys /)
     geopotential_height_wrt_surface_at_interface(1,:) = (/ 3000.0_kind_phys, 1000.0_kind_phys, 0.0_kind_phys /)
     geopotential_height_wrt_surface_at_interface(2,:) = (/ 3000.0_kind_phys, 500.0_kind_phys, -1500.0_kind_phys /)
     surface_temperature = (/ 300.0_kind_phys, 300.0_kind_phys /)
     surface_geopotential = (/ 100.0_kind_phys, 200.0_kind_phys /)
+    surface_albedo = 0.10_kind_phys
     standard_gravitational_acceleration = 10.0_kind_phys
     temperature(:,1) = (/ 100._kind_phys, 200._kind_phys /)
     temperature(:,2) = (/ 300._kind_phys, 400._kind_phys /)
@@ -98,7 +205,8 @@ contains
       call constituent_props_ptr(i)%set(const_prop, errcode, errmsg)
     end do
 
-    call musica_ccpp_init(NUM_LAYERS, NUM_LAYERS+1, errmsg, errcode)
+    call musica_ccpp_init(NUM_LAYERS, NUM_LAYERS+1, photolysis_wavelength_grid_interfaces, &
+                          errmsg, errcode)
     if (errcode /= 0) then
       write(*,*) trim(errmsg)
       stop 3
@@ -113,10 +221,11 @@ contains
     write(*,*) "[MUSICA INFO] Initial Concentrations"
     write(*,fmt="(4(3x,e13.6))") constituents
 
-    call musica_ccpp_run(time_step, temperature, pressure, dry_air_density, constituent_props_ptr, &
-                         constituents, geopotential_height_wrt_surface_at_midpoint,                &
-                         geopotential_height_wrt_surface_at_interface, surface_temperature,        &
-                         surface_geopotential, standard_gravitational_acceleration, errmsg, errcode)
+    call musica_ccpp_run(time_step, temperature, pressure, dry_air_density, constituent_props_ptr,  &
+                         constituents, geopotential_height_wrt_surface_at_midpoint,                 &
+                         geopotential_height_wrt_surface_at_interface, surface_temperature,         &
+                         surface_geopotential, surface_albedo, standard_gravitational_acceleration, &
+                         errmsg, errcode)
     if (errcode /= 0) then
       write(*,*) trim(errmsg)
       stop 3
@@ -131,6 +240,8 @@ contains
       write(*,*) trim(errmsg)
       stop 3
     endif
+
+    deallocate(constituent_props_ptr)
 
   end subroutine test_musica_ccpp_api
 
