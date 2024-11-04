87a45418a647a08fc9626b1f04c8d6c0e460bfc0
Haipeng Lin <hplin@ucar.edu>
2024-11-04 14:33:34 -0500

    Add adiabatic scheme; check_energy calls to CAM7; check_energy_fix compatibility with check_energy_chng

--- schemes/check_energy/check_energy_fix.F90
+++ schemes/check_energy/check_energy_fix.F90
@@ -11,7 +11,7 @@ contains
   ! Add heating rate required for global mean total energy conservation
 !> \section arg_table_check_energy_fix_run Argument Table
 !! \htmlinclude arg_table_check_energy_fix_run.html
-  subroutine check_energy_fix_run(ncol, pver, pint, rga, heat_glob, ptend_s, eshflx)
+  subroutine check_energy_fix_run(ncol, pver, pint, rga, heat_glob, ptend_s, eshflx, scheme_name)
     ! Input arguments
     integer,            intent(in)    :: ncol           ! number of atmospheric columns
     integer,            intent(in)    :: pver           ! number of vertical layers
@@ -20,9 +20,15 @@ contains
     real(kind_phys),    intent(in)    :: heat_glob      ! global mean heating rate [J kg-1 s-1]
     real(kind_phys),    intent(out)   :: ptend_s(:,:)   ! physics tendency heating rate [J kg-1 s-1]
     real(kind_phys),    intent(out)   :: eshflx(:)      ! effective sensible heat flux [W m-2]
+                                                        ! for check_energy_chng
+
+    character(len=64),  intent(out)   :: scheme_name    ! scheme name
 
     ! Local variables
     integer :: i
+
+    ! Set scheme name for check_energy_chng
+    scheme_name = "check_energy_fix"
 
     ! add (-) global mean total energy difference as heating
     ptend_s(:ncol, :pver) = heat_glob
