7b188e35fe26247289d3bdbbbdd902cde382765a
Haipeng Lin <hplin@ucar.edu>
2024-11-15 12:46:25 -0500

    Address review comments

--- schemes/sima_diagnostics/check_energy_gmean_diagnostics.F90
+++ schemes/sima_diagnostics/check_energy_gmean_diagnostics.F90
@@ -1,13 +1,8 @@
 ! Diagnostic scheme for check_energy_gmean
-! This replicates the "debug printout" with the global mean total energy in CAM:
-!  nstep, te        0   0.25872620595082335E+10   0.25872620595082335E+10  -0.00000000000000000E+00   0.10126767279027148E+06   0.21940670000000080E+03
-! where the numbers correspond to:
-! - timestep number
+! This creates a debug printout with:
 ! - global mean input energy using dycore energy formula
 ! - global mean output energy at end of physics timestep using dycore energy formula
 ! - global mean heating rate for energy conservation
-! - global mean surface pressure
-! - global mean model top pressure
 ! These numbers are very useful for matching models bit-for-bit because they include "everything" in the model.
 module check_energy_gmean_diagnostics
    use ccpp_kinds, only: kind_phys
@@ -41,17 +36,13 @@ contains
       teinp_glob, &
       teout_glob, &
       heat_glob,  &
-      psurf_glob, &
-      ptopb_glob, &
       errmsg, errflg)
 
-      logical,            intent(in)  :: amIRoot
+      logical,            intent(in)  :: amIRoot       ! are we on the MPI root task?
       integer,            intent(in)  :: iulog         ! log output unit
 
       real(kind_phys),    intent(in)  :: teinp_glob    ! global mean energy of input state [J m-2]
       real(kind_phys),    intent(in)  :: teout_glob    ! global mean energy of output state [J m-2]
-      real(kind_phys),    intent(in)  :: psurf_glob    ! global mean surface pressure [Pa]
-      real(kind_phys),    intent(in)  :: ptopb_glob    ! global mean top boundary pressure [Pa]
       real(kind_phys),    intent(in)  :: heat_glob     ! global mean heating rate [J kg-1 s-1]
 
       character(len=512), intent(out) :: errmsg
