0af2e2b80bc2f8597b685bf08b0917233ce84afa
Jesse Nusbaumer <nusbaume@ucar.edu>
2025-05-02 11:06:43 -0600

    Initial file reader object attempt (#240)
    Originator(s): @mwaxmonsky @nusbaume 
    
    Description (include issue title and the keyword ['closes', 'fixes',
    'resolves'] and issue number):
    
    This PR contains a File I/O class as designed by myself and (mostly)
    @mwaxmonsky. This current version should be able
    to read in any integer, real, or character-type variable from a NetCDF
    file, up to a five-dimensional array. Currently
    the entire variable will be read, but a future PR will also allow for
    variable subsetting, particularly for the record dimension
    (as that is required for multiple CAM-SIMA use cases).
    
    A few notes on the design:
    
    1. The major goal of this work is to create a File I/O interface that is
    host model independent. We believe
    we have accomplished that but if you see anything that appears
    SIMA-specific please let us know!
    
    2. The class is designed to allocate a variable's memory on the host
    model side. This was done with the idea
    that a host model might have a more sophisticated way of managing memory
    then just putting it on the heap (e.g. a memory pool).
    
    3. We were torn on how many capabilities the FileIO class should have.
    We decided to keep the functionality low to reduce
    complexity, but if there are strong feelings otherwise please let us
    know! In particular we are leaning towards making
    the necessary regridding/interpolation steps a separate class, so that
    we don't have too much inter-mingling of functionality
        in a single location.
    
    Anyways, any thoughts, comments, suggestions, or review requests would
    be greatly appreciated! The corresponding CAM-SIMA PR is
    ESCOMP/CAM-SIMA#385. Thanks!
    
    Fixes #241
    
    List all namelist files that were added or changed:
    
    A       test/test_schemes/file_io_test_namelist.xml
    - Namelist file for file I/O test scheme which can be used to specify
    the test NetCDF file(s).
    
    List all files eliminated and why: N/A (no files removed)
    
    List all files added and what they do:
    
    A       phys_utils/ccpp_io_reader.F90
     - Initial File I/O reader type declarations
     
    A       test/test_schemes/file_io_test.F90
    A       test/test_schemes/file_io_test.meta
    A       test/test_suites/suite_file_io_test.xml
    - File I/O test scheme and suite for use in CAM-SIMA testing (will be
    removed once a more complete/automated testing system has been
    developed).
    
    List all existing files that have been modified, and describe the
    changes: N/A (no files were modified).
    (Helpful git command: `git diff --name-status
    development...<your_branch_name>`)
    
    List all automated tests that failed, as well as an explanation for why
    they weren't fixed: All tests pass.
    
    Is this an answer-changing PR? If so, is it a new physics package,
    algorithm change, tuning change, etc?
    
    If yes to the above question, describe how this code was validated with
    the new/modified features:
    
    ---------
    
    Co-authored-by: Michael Waxmonsky <mwaxmonsky@ucar.edu>

---
+++ test/test_schemes/file_io_test.F90
@@ -0,0 +1,116 @@
+module file_io_test
+
+!This test scheme exists to exercise the
+!abstract File I/O interface designed for
+!use by host-agnostic CCPP schemes.
+
+!The test code below use input files
+!needed by the RRTMGP radiation scheme.
+
+implicit none
+private
+
+public :: file_io_test_init
+public :: file_io_test_run
+
+contains
+
+!> \section arg_table_file_io_test_init  Argument Table
+!! \htmlinclude file_io_test_init.html
+  subroutine file_io_test_init(file_path, errmsg, errcode)
+
+     !Portable use statements:
+     use ccpp_kinds, only: kind_phys
+     use ccpp_io_reader, only: abstract_netcdf_reader_t, create_netcdf_reader_t
+
+     !Input variables:
+     character(len=*),   intent(in)  :: file_path
+
+     !Output variables:
+     character(len=512), intent(out) :: errmsg
+     integer,            intent(out) :: errcode
+
+     !Variables read from file:
+     character(len=:), pointer :: gas_names(:)
+     integer, pointer          :: band2gpt(:,:)
+     real(kind_phys), pointer  :: press_ref(:)
+     real(kind_phys), pointer  :: band_lims_wavenum(:,:)
+
+     class(abstract_netcdf_reader_t), allocatable :: reader
+
+     !NetCDF dimensions:
+     integer :: bnd
+     integer :: absorber
+
+     reader = create_netcdf_reader_t()
+
+     !Initialize output variables:
+     errcode = 0
+     errmsg  = ''
+
+     ! Open file
+     call reader%open_file(file_path, errmsg, errcode)
+     if (errcode /= 0) then
+        return !Error has occurred, so exit scheme
+     end if
+
+
+     !Read variables from NetCDF file:
+     !-----------------------
+
+     !Attempt to get absorbing gas names from file:
+     call reader%get_var('gas_names', gas_names, errmsg, errcode)
+     if (errcode /= 0) then
+        return !Error has occurred, so exit scheme
+     end if
+
+     !Attempt to get reference pressure from file:
+     call reader%get_var('press_ref', press_ref, errmsg, errcode)
+     if (errcode /= 0) then
+        return !Error has occurred, so exit scheme
+     end if
+
+     !Attempt to get wavenumber band grid start/end indices from file:
+     call reader%get_var('bnd_limits_gpt', band2gpt, errmsg, errcode)
+     if (errcode /= 0) then
+        return !Error has occurred, so exit scheme
+     end if
+
+     !Attempt to get wavenumber band start/end values from file:
+     call reader%get_var('bnd_limits_wavenumber', band_lims_wavenum, errmsg, errcode)
+     if (errcode /= 0) then
+        return !Error has occurred, so exit scheme
+     end if
+
+     ! Close file
+     call reader%close_file(errmsg, errcode)
+     if (errcode /= 0) then
+        return !Error has occurred, so exit scheme
+     end if
+
+     !Write array shape information:
+     write(*,*) 'gas_names length', len(gas_names)
+     write(*,*) 'gas_names shape ', shape(gas_names)
+     write(*,*) 'press_ref shape ', shape(press_ref)
+     write(*,*) 'band2gpt shape ', shape(band2gpt)
+     write(*,*) 'band_lims_wavenum ', shape(band_lims_wavenum)
+
+     !Write max values to stdout:
+     write(*,*) 'First absorbing gas name = ', gas_names(1)
+     write(*,*) 'Max RRTMGP reference pressure value = ', maxval(press_ref)
+     write(*,*) 'Max RRTMGP band starting wave idx   = ', maxval(band2gpt(1,:))
+     write(*,*) 'Max RRTMGP band starting wavenumber = ', maxval(band_lims_wavenum(1,:))
+
+  end subroutine file_io_test_init
+
+!> \section arg_table_file_io_test_run  Argument Table
+!! \htmlinclude file_io_test_run.html
+  subroutine file_io_test_run(errmsg, errcode)
+     character(len=512), intent(out) :: errmsg
+     integer,            intent(out) :: errcode
+
+     errcode = 0
+     errmsg = ''
+
+  end subroutine file_io_test_run
+end module file_io_test
