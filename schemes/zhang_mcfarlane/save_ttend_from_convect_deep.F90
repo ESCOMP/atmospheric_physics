088500617071f99ed7b8c953e6757b2f484f94ee
Haipeng Lin <hplin@ucar.edu>
2025-10-14 15:20:14 -0400

    Update to accummulate ttend_dp

--- schemes/zhang_mcfarlane/save_ttend_from_convect_deep.F90
+++ schemes/zhang_mcfarlane/save_ttend_from_convect_deep.F90
@@ -2,16 +2,33 @@
 ! for use by gravity wave parameterization
 !
 ! This scheme has to be run after all deep convective schemes,
-! but before the final tendencies are applied.
+! before the final tendencies are applied.
+! Since ZM applies tendencies in each sub-scheme, this scheme has
+! to run repeatedly to archive all tendencies from ZM.
 module save_ttend_from_convect_deep
   use ccpp_kinds, only: kind_phys
 
   implicit none
   private
 
+  public :: save_ttend_from_convect_deep_timestep_init
   public :: save_ttend_from_convect_deep_run
 
 contains
+
+!> \section arg_table_save_ttend_from_convect_deep_timestep_init Argument Table
+!! \htmlinclude save_ttend_from_convect_deep_timestep_init.html
+  subroutine save_ttend_from_convect_deep_timestep_init(ncol, pver, ttend_dp, errmsg, errflg)
+    integer,            intent(in)  :: ncol
+    integer,            intent(in)  :: pver
+
+    real(kind_phys),    intent(out) :: ttend_dp(:, :)          ! Temperature tendency from deep convection [K s-1]
+    character(len=512), intent(out) :: errmsg
+    integer,            intent(out) :: errflg
+
+    ttend_dp(:,:) = 0._kind_phys
+
+  end subroutine save_ttend_from_convect_deep_timestep_init
 
 !> \section arg_table_save_ttend_from_convect_deep_run Argument Table
 !! \htmlinclude save_ttend_from_convect_deep_run.html
@@ -22,13 +39,15 @@ contains
     errmsg, errflg)
 
     ! Input arguments
-    integer,            intent(in)  :: ncol                     ! Number of atmospheric columns [count]
-    integer,            intent(in)  :: pver                     ! Number of vertical layers [count]
+    integer,            intent(in)  :: ncol
+    integer,            intent(in)  :: pver
     real(kind_phys),    intent(in)  :: tend_s(:, :)            ! Enthalpy tendency from deep convection [J kg-1 s-1]
     real(kind_phys),    intent(in)  :: cpair                   ! Specific heat of dry air at constant pressure [J kg-1 K-1]
 
+    ! Input/output arguments
+    real(kind_phys),    intent(inout) :: ttend_dp(:, :)         ! Temperature tendency from deep convection [K s-1] (accummulated)
+
     ! Output arguments
-    real(kind_phys),    intent(out) :: ttend_dp(:, :)          ! Temperature tendency from deep convection [K s-1]
     character(len=512), intent(out) :: errmsg
     integer,            intent(out) :: errflg
 
@@ -41,7 +60,7 @@ contains
     ! Convert to temperature tendency
     do k = 1, pver
       do i = 1, ncol
-        ttend_dp(i, k) = tend_s(i, k) / cpair
+        ttend_dp(i, k) = ttend_dp(i, k) + tend_s(i, k) / cpair
       end do
     end do
 
