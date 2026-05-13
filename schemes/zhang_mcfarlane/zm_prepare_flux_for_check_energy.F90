9cd83840c5135c0e8ec5434d55420cc225730a02
Haipeng Lin <jimmie.lin@gmail.com>
2026-05-13 07:55:56 -0400

    After removing rice

--- schemes/zhang_mcfarlane/zm_prepare_flux_for_check_energy.F90
+++ schemes/zhang_mcfarlane/zm_prepare_flux_for_check_energy.F90
@@ -1,5 +1,5 @@
 ! Prepare flux variables for energy checker after ZM deep convection
-! Has to run after both zm_convr (provides reserved liquid and reserved ice)
+! Has to run after both zm_convr (provides reserved liquid)
 ! and zm_conv_evap+interstitial to deep (provides snow_dp and prec_dp) have
 ! been ran.
 ! The resulting net liquid and (lwe) ice fluxes are combined and provided to
@@ -19,7 +19,7 @@ contains
 !! \htmlinclude zm_prepare_flux_for_check_energy_run.html
   subroutine zm_prepare_flux_for_check_energy_run( &
     ncol, &
-    prec_dp, rliq, snow_dp, rice, &
+    prec_dp, rliq, snow_dp, &
     scheme_name, &
     flx_cnd, flx_ice, &
     errmsg, errflg)
@@ -29,12 +29,11 @@ contains
     real(kind_phys),    intent(in)  :: prec_dp(:)             ! Deep convection precipitation rate [m s-1]
     real(kind_phys),    intent(in)  :: rliq(:)                ! Reserved liquid water tendency [m s-1]
     real(kind_phys),    intent(in)  :: snow_dp(:)             ! Deep convection frozen precipitation rate [m s-1]
-    real(kind_phys),    intent(in)  :: rice(:)                ! Reserved ice tendency [m s-1]
 
     ! Output arguments
     character(len=16),  intent(out) :: scheme_name            ! Scheme name for energy checking
-    real(kind_phys),    intent(out) :: flx_cnd(:)             ! Combined liquid and ice fluxes [m s-1]
-    real(kind_phys),    intent(out) :: flx_ice(:)             ! Combined ice fluxes [m s-1]
+    real(kind_phys),    intent(out) :: flx_cnd(:)             ! Liquid fluxes [m s-1]
+    real(kind_phys),    intent(out) :: flx_ice(:)             ! Ice fluxes [m s-1]
     character(len=512), intent(out) :: errmsg
     integer,            intent(out) :: errflg
 
@@ -51,9 +50,8 @@ contains
     ! This represents net liquid and ice fluxes through column boundaries
     flx_cnd(:ncol) = prec_dp(:ncol) + rliq(:ncol)
 
-    ! Combine frozen precipitation and reserved ice for total ice flux
-    ! This represents net ice fluxes through column boundaries
-    flx_ice(:ncol) = snow_dp(:ncol) + rice(:ncol)
+    ! Frozen precipitation represents net ice fluxes through column boundaries
+    flx_ice(:ncol) = snow_dp(:ncol)
 
   end subroutine zm_prepare_flux_for_check_energy_run
 
