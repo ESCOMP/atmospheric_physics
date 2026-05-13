3b3e8a76cdd1156854a97173f66ec654f1406da6
Haipeng Lin <hplin@ucar.edu>
2026-05-13 15:06:19 -0400

    Initial attempt for converted beljaars_drag, interstitials, diagnostics

--- schemes/vertical_diffusion/diffusion_stubs.F90
+++ schemes/vertical_diffusion/diffusion_stubs.F90
@@ -9,16 +9,12 @@ module diffusion_stubs
 
   implicit none
   private
-  save
 
   ! CCPP-compliant subroutines
   public :: zero_upper_boundary_condition_init
 
   public :: tms_beljaars_zero_stub_run
   public :: turbulent_mountain_stress_add_drag_coefficient_run
-  public :: beljaars_add_wind_damping_rate_run
-
-  public :: beljaars_add_updated_residual_stress_run
   public :: turbulent_mountain_stress_add_updated_surface_stress_run
 
   public :: vertical_diffusion_not_use_rairv_init
@@ -138,108 +134,6 @@ contains
     ksrf(:ncol) = ksrf(:ncol) + ksrftms(:ncol)
 
   end subroutine turbulent_mountain_stress_add_drag_coefficient_run
-
-  ! Add Beljaars drag to the wind damping rate for vertical diffusion.
-  ! Has to run after vertical_diffusion_wind_damping_rate
-!> \section arg_table_beljaars_add_wind_damping_rate_run Argument Table
-!! \htmlinclude arg_table_beljaars_add_wind_damping_rate_run.html
-  subroutine beljaars_add_wind_damping_rate_run( &
-    ncol, pver, &
-    dragblj, &
-    tau_damp_rate, &
-    errmsg, errflg)
-
-    ! Input arguments
-    integer,            intent(in)    :: ncol
-    integer,            intent(in)    :: pver
-    real(kind_phys),    intent(in)    :: dragblj(:, :)    ! Drag profile from Beljaars SGO form drag > 0. [s-1]
-
-    ! Input/output arguments
-    real(kind_phys),    intent(inout) :: tau_damp_rate(:,:) ! Rate at which external (surface) stress damps wind speeds [s-1]
-
-    ! Output arguments
-    character(len=512), intent(out)   :: errmsg  ! error message
-    integer,            intent(out)   :: errflg  ! error flag
-
-    errmsg = ''
-    errflg = 0
-
-    ! Beljaars et al SGO scheme incorporated here. It
-    ! appears as a "3D" tau_damp_rate specification.
-    tau_damp_rate(:ncol,:pver) = tau_damp_rate(:ncol,:pver) + dragblj(:ncol,:pver)
-
-  end subroutine beljaars_add_wind_damping_rate_run
-
-  ! Add Beljaars stress using updated provisional winds to surface stresses used
-  ! for horizontal momentum diffusion - kinetic energy dissipation.
-!> \section arg_table_beljaars_add_updated_residual_stress_run Argument Table
-!! \htmlinclude arg_table_beljaars_add_updated_residual_stress_run.html
-  subroutine beljaars_add_updated_residual_stress_run( &
-    ncol, pver, &
-    gravit, &
-    p, &
-    do_iss, &
-    itaures, &
-    dragblj, &
-    u1, v1, & ! provisionally updated winds
-    ! input/output
-    tauresx, tauresy, &
-    errmsg, errflg)
-
-    use coords_1d, only: Coords1D
-
-    ! Input arguments
-    integer,         intent(in)       :: ncol
-    integer,         intent(in)       :: pver
-    real(kind_phys), intent(in)       :: gravit
-    type(coords1d),  intent(in)       :: p                ! Pressure coordinates [Pa]
-    logical,         intent(in)       :: do_iss           ! Flag for implicit surface stress (namelist from vdiff)
-    logical,         intent(in)       :: itaures          ! Flag for updating tauresx tauresy in this subroutine.
-    real(kind_phys), intent(in)       :: dragblj(:, :)    ! Drag profile from Beljaars SGO form drag > 0. [s-1]
-    real(kind_phys), intent(in)       :: u1(:,:)          ! After vertical diffusion u-wind [m s-1]
-    real(kind_phys), intent(in)       :: v1(:,:)          ! After vertical diffusion v-wind [m s-1]
-
-    ! Input/output arguments
-    real(kind_phys), intent(inout)    :: tauresx(:)       ! Partially updated residual surface stress
-    real(kind_phys), intent(inout)    :: tauresy(:)
-    character(len=512), intent(out)   :: errmsg  ! error message
-    integer,            intent(out)   :: errflg  ! error flag
-
-    integer :: i, k
-    real(kind_phys) :: taubljx(ncol)             ! recomputed explicit/residual beljaars stress
-    real(kind_phys) :: taubljy(ncol)             ! recomputed explicit/residual beljaars stress
-
-    errmsg = ''
-    errflg = 0
-
-    if(.not. do_iss) then
-      ! Not added to residual stress if implicit surface stress is off
-      return
-    endif
-
-    if(.not. itaures) then
-      ! Not added to residual stress if residual stress is not requested to be updated.
-      return
-    endif
-
-    do i = 1, ncol
-      ! Add vertically-integrated Beljaars drag to residual stress.
-      ! note: in vertical_diffusion_tend, tautotx has taubljx added to it
-      ! but this is computed separately using UPDATED u, v (and is not written back to pbuf)
-      ! there is a FIXME in beljaars_drag that notes maybe updated u, v could be used there
-      ! but that is too early (before vdiff). Keeping this for now. hplin 5/22/25
-      taubljx(i) = 0._kind_phys
-      taubljy(i) = 0._kind_phys
-      do k = 1, pver
-        taubljx(i) = taubljx(i) + (1._kind_phys/gravit)*dragblj(i, k)*u1(i, k)*p%del(i, k)
-        taubljy(i) = taubljy(i) + (1._kind_phys/gravit)*dragblj(i, k)*v1(i, k)*p%del(i, k)
-      end do
-    enddo
-
-    tauresx(:ncol) = tauresx(:ncol) + taubljx(:ncol)
-    tauresy(:ncol) = tauresy(:ncol) + taubljy(:ncol)
-
-  end subroutine beljaars_add_updated_residual_stress_run
 
   ! Add TMS using updated provisional winds to total surface stress in
   ! horizontal momentum diffusion - kinetic energy dissipation.
