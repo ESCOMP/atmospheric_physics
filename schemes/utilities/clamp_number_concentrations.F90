9b6e450e751096276e91c7fb54c15221e87adf1f
Haipeng Lin <hplin+github@ucar.edu>
2026-07-17 11:08:29 -0400

    More robust fixes for the clamp and initialize; dimensions_pre unit fix

--- schemes/utilities/clamp_number_concentrations.F90
+++ schemes/utilities/clamp_number_concentrations.F90
@@ -28,11 +28,11 @@ module clamp_number_concentrations
   real(kind_phys), parameter :: qmax = 1.0e10_kind_phys  ! maximum number concentration [kg-1]
 
   ! Standard names for each species (order matches ix_ variables above)
-  character(len=128), parameter :: std_names(num_species) = &
-    (/'mass_number_concentration_of_cloud_liquid_water_droplets_in_moist_air_and_condensed_water', &
-      'mass_number_concentration_of_rain_drops_in_moist_air_and_condensed_water        ', &
-      'mass_number_concentration_of_ice_wrt_moist_air_and_condensed_water         ', &
-      'mass_number_concentration_of_snow_crystals_in_moist_air_and_condensed_water        '/)
+  character(len=*), parameter :: std_names(num_species) = &
+    [character(len=128) :: 'mass_number_concentration_of_cloud_liquid_water_droplets_in_moist_air_and_condensed_water', &
+                           'mass_number_concentration_of_rain_drops_in_moist_air_and_condensed_water', &
+                           'mass_number_concentration_of_ice_wrt_moist_air_and_condensed_water', &
+                           'mass_number_concentration_of_snow_crystals_in_moist_air_and_condensed_water']
 
 contains
 
