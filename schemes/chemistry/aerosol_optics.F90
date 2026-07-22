1d7906b15b07aee850add2312dca4fb32a778c67
Haipeng Lin <hplin+github@ucar.edu>
2026-07-22 17:56:08 -0400

    Update aerosol_optics to the new aerosol_state wet_volume/water_volume interface.
    The diagnostic-list water uptake in modal_aerosol_state (CAM-SIMA) takes
    t, pmid, h2ommr, cldn and top_lev; thread them through the scheme and
    the two per-species diagnostic calls.

--- schemes/chemistry/aerosol_optics.F90
+++ schemes/chemistry/aerosol_optics.F90
@@ -150,7 +150,7 @@ contains
     rga, &
     idx_sw_diag, &
     num_bulk_aer, &
-    relh, pdeldry, constituents, &
+    relh, pdeldry, t, pmid, h2ommr, cldn, constituents, &
     aer_tau, aer_tau_w, aer_tau_w_g, aer_lw_abs, &
     dustaod, sulfaod, bcaod, pomaod, soaaod, ssltaod, &
     aodabsbc, &
@@ -190,6 +190,10 @@ contains
     integer,            intent(in)  :: num_bulk_aer    ! number of bulk aerosol constituents in climate list [count]
     real(kind_phys),    intent(in)  :: relh(:, :)      ! relative humidity [fraction]
     real(kind_phys),    intent(in)  :: pdeldry(:, :)   ! dry air pressure thickness [Pa]
+    real(kind_phys),    intent(in)  :: t(:, :)         ! air temperature [K]
+    real(kind_phys),    intent(in)  :: pmid(:, :)      ! air pressure at layer midpoint [Pa]
+    real(kind_phys),    intent(in)  :: h2ommr(:, :)    ! water vapor mixing ratio [kg kg-1]
+    real(kind_phys),    intent(in)  :: cldn(:, :)      ! layer cloud fraction [fraction]
     real(kind_phys),    intent(in)  :: constituents(:, :, :)
 
     ! Output arguments
@@ -357,7 +361,8 @@ contains
         call aerosol_optics_sw_bin(aeroprops, aerostate, ibin, &
                                    ncol, pver, top_lev, nswbands, nlwbands, nrh, &
                                    idx_sw_diag, &
-                                   relh, sulfwtpct, mass, crefwsw, crefwlw, &
+                                   relh, sulfwtpct, t, pmid, h2ommr, cldn, &
+                                   mass, crefwsw, crefwlw, &
                                    geometric_radius, &
                                    tau_bin, ssa_bin, asm_bin, &
                                    pabs_vis, dopaer0_vis, &
@@ -382,8 +387,10 @@ contains
         !-------------------------------------------------
         ! Per-species diagnostic accumulation at vis band
         !-------------------------------------------------
-        wetvol_d(:ncol, :) = aerostate%wet_volume(aeroprops, ibin, ncol, pver)
-        watervol_d(:ncol, :) = aerostate%water_volume(aeroprops, ibin, ncol, pver)
+        wetvol_d(:ncol, :) = aerostate%wet_volume(aeroprops, ibin, ncol, pver, top_lev, &
+                                                  t, pmid, h2ommr, cldn)
+        watervol_d(:ncol, :) = aerostate%water_volume(aeroprops, ibin, ncol, pver, top_lev, &
+                                                      t, pmid, h2ommr, cldn)
 
         do ilev = top_lev, pver
           ! Reset per-species accumulators for this bin/level
@@ -534,8 +541,9 @@ contains
         ! Call the portable core for longwave optics calculation per-bin:
         !-------------------------------------------------
         call aerosol_optics_lw_bin(aeroprops, aerostate, ibin, &
-                                   ncol, pver, nswbands, nlwbands, nrh, &
-                                   relh, sulfwtpct, mass, crefwsw, crefwlw, &
+                                   ncol, pver, top_lev, nswbands, nlwbands, nrh, &
+                                   relh, sulfwtpct, t, pmid, h2ommr, cldn, &
+                                   mass, crefwsw, crefwlw, &
                                    geometric_radius, &
                                    tau_lw_bin, absorp_bin, &
                                    errmsg, errflg)
